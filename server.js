4188655371248c8a026ce7bdda35bb53c4b4d4d8
zarkoselak <zarko@extensionengine.com>
2015-11-07 09:13:28 +0100

    [Update] fs_r - project setup - test route

--- server.js
+++ server.js
@@ -4,3 +4,16 @@ var express 	= require('express');
 var bodyParser 	= require('body-parser');
 
 var app = express(); 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port 	= process.env.PORT || 8585;
+var router 	= express.Router();
+
+router.get('/', function(req, res) {
+	res.json({ message: 'API!' });
+});
+
+app.use('/api', router);
+app.listen(port);
